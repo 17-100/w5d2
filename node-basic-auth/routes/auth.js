81ed04e31d3eb1516889fa89d1e60479e8ee2a1b
Jan Redmann <janredmann1@gmail.com>
2021-02-09 17:03:31 +0100

    add node-basic-auth sign up

---
+++ node-basic-auth/routes/auth.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
+
+// signup
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+// the signup form posts to this route
+router.post('/signup', (req, res) => {
+  // get username and password
+  const { username, password } = req.body;
+  console.log(username, password);
+  // is the password longer than 8 chars and the username not empty
+  if (password.length < 8) {
+    // if not show the signup again with a message
+    res.render('signup', { message: 'Your password has to be 8 chars min' });
+    return
+  }
+  if (username === '') {
+    res.render('signup', { message: 'Your username cannot be empty' });
+    return
+  }
+  // check if the username already exists
+  User.findOne({ username: username })
+    .then(userFromDB => {
+      if (userFromDB !== null) {
+        // if yes show the signup again with a message
+        res.render('signup', { message: 'Username is already taken' });
+      } else {
+        // all validation passed - > we can create a new user in the database with a hashed password
+        // create salt and hash
+        const salt = bcrypt.genSaltSync();
+        const hash = bcrypt.hashSync(password, salt)
+        // create the user in the db
+        User.create({ username: username, password: hash })
+          .then(userFromDB => {
+            console.log(userFromDB);
+            // then redirect to login
+            res.redirect('/');
+          })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+module.exports = router;
